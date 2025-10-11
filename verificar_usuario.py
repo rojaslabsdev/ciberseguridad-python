80fc566b7a4fa8c0a54db7749daae5997d308db8
rojaslabsdev <bryan.rojas@upaep.edu.mx>
2025-10-10 19:41:29 -0600

    Update verificar_usuario.py

--- verificar_usuario.py
+++ verificar_usuario.py
@@ -1,5 +1,5 @@
 
-usuario = input("Ingresa tu alias de hacker: ")
+usuario = input("Ingresa tu alias : ")
 rol = input("Ingresa tu rol (admin/usuario): ")
 if not usuario:
     print("Error: Ingresa un alias.")
