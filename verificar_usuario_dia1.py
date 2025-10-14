6b63e9b530f7517934233f8b9aafd7bd4a4bca6e
rojaslabsdev <bryan.rojas@upaep.edu.mx>
2025-10-13 22:25:38 -0600

    Delete verificar_usuario_dia1.py

--- verificar_usuario_dia1.py
+++
@@ -1,7 +0,0 @@
-
-usuario = input("Ingresa tu alias : ")
-rol = input("Ingresa tu rol (admin/usuario): ")
-if not usuario:
-    print("Error: Ingresa un alias.")
-else:
-    print(f"Acceso para {usuario}: {'Autorizado' if rol.lower() == 'admin' else 'Restringido'}")
