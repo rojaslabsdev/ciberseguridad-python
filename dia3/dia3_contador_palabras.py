66ad70abfdec5d45d7276869c299d273f18bdd37
rojaslabsdev <bryan.rojas@upaep.edu.mx>
2025-10-13 22:14:23 -0600

    Add files via upload

---
+++ dia3/dia3_contador_palabras.py
@@ -0,0 +1,7 @@
+
+palabra = input("Dame una Palabra:  ")
+numero = int(input("Cuantas veces quieres repetir la palabra?: "))
+
+
+for i in range(numero):
+    print(palabra)
---
+++ dia3/dia3_practica.py
@@ -0,0 +1,12 @@
+usuario_correcto = "bryan"
+password_correcto = "1234"
+
+while True:
+    usuario = input("Usuario: ")
+    password = input("Contraseña: ")
+
+    if usuario == usuario_correcto and password == password_correcto:
+        print("✅ Bienvenido,", usuario)
+        break
+    else:
+        print("❌ Datos incorrectos, intenta de nuevo.")
